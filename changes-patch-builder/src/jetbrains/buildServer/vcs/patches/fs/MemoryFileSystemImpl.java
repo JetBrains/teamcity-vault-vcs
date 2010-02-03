383e9336edbef13b108830d577681f528fbe9022
Victory.Bedrosova <victory.bedrosova@jetbrains.com>
2010-02-03 11:53:32 +0000

    update and add copyright with 2010

--- changes-patch-builder/src/jetbrains/buildServer/vcs/patches/fs/MemoryFileSystemImpl.java
+++ changes-patch-builder/src/jetbrains/buildServer/vcs/patches/fs/MemoryFileSystemImpl.java
@@ -24,6 +24,7 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
 package jetbrains.buildServer.vcs.patches.fs;
 
 import jetbrains.buildServer.vcs.patches.util.Assert;
