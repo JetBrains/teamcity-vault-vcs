2a762fda03628b2e8c8c8736aea2d9ffd8b048ae
Eugene.Petrenko <Eugene.Petrenko@jetbrains.com>
2012-10-19 16:02:19 +0000

    @NotNull around patcher* modules

--- changes-patch-builder-test/src/jetbrains/buildServer/vcs/patches/PatchBuilderMock.java
+++ changes-patch-builder-test/src/jetbrains/buildServer/vcs/patches/PatchBuilderMock.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * User: vbedrosova
@@ -27,53 +28,53 @@ import java.util.ArrayList;
  * Time: 15:58:06
  */
 public class PatchBuilderMock implements PatchBuilder {
-  private ArrayList<String> myOperations = new ArrayList<String>();
+  private final ArrayList<String> myOperations = new ArrayList<String>();
 
   public ArrayList<String> getOperations() {
     return myOperations;
   }
   
-  public void deleteFile(File file, boolean b) throws IOException {
+  public void deleteFile(@NotNull File file, boolean b) throws IOException {
     myOperations.add("DELETE " + unifyPath(file.getPath()));
   }
 
-  public void deleteDirectory(File file, boolean b) throws IOException {
+  public void deleteDirectory(@NotNull File file, boolean b) throws IOException {
     myOperations.add("DELETE_DIR " + unifyPath(file.getPath()));
   }
 
-  public void changeOrCreateTextFile(File file, String s, InputStream inputStream, long l, byte[] bytes) throws IOException {
+  public void changeOrCreateTextFile(@NotNull File file, String s, @NotNull InputStream inputStream, long l, byte[] bytes) throws IOException {
     myOperations.add("WRITE_TEXT " + unifyPath(file.getPath()) + " " + s);
   }
 
-  public void changeOrCreateBinaryFile(File file, String s, InputStream inputStream, long l) throws IOException {
+  public void changeOrCreateBinaryFile(@NotNull File file, String s, @NotNull InputStream inputStream, long l) throws IOException {
     myOperations.add("WRITE " + unifyPath(file.getPath()) + " " + s);
   }
 
-  public void createDirectory(File file) throws IOException {
+  public void createDirectory(@NotNull File file) throws IOException {
     myOperations.add("CREATE_DIR " + unifyPath(file.getPath()));
   }
 
-  public void createBinaryFile(File file, String s, InputStream inputStream, long l) throws IOException {
+  public void createBinaryFile(@NotNull File file, String s, @NotNull InputStream inputStream, long l) throws IOException {
     myOperations.add("CREATE " + unifyPath(file.getPath()) + " " + s);
   }
 
-  public void createTextFile(File file, String s, InputStream inputStream, long l, byte[] bytes) throws IOException {
+  public void createTextFile(@NotNull File file, String s, @NotNull InputStream inputStream, long l, byte[] bytes) throws IOException {
     myOperations.add("CREATE_TEXT " + unifyPath(file.getPath()) + " " + s);
   }
 
-  public void renameFile(File file, File file1, boolean b) throws IOException {
+  public void renameFile(@NotNull File file, @NotNull File file1, boolean b) throws IOException {
     myOperations.add("RENAME " + unifyPath(file.getPath()));
   }
 
-  public void renameDirectory(File file, File file1, boolean b) throws IOException {
+  public void renameDirectory(@NotNull File file, @NotNull File file1, boolean b) throws IOException {
     myOperations.add("RENAME_DIR " + unifyPath(file.getPath()));
   }
 
-  public void setWorkingDirectory(File file, boolean b) throws IOException {
+  public void setWorkingDirectory(@NotNull File file, boolean b) throws IOException {
     myOperations.add("SET_WORKING_DIR " + unifyPath(file.getPath()));
   }
 
-  public void setLastModified(File file, long l) throws IOException {
+  public void setLastModified(@NotNull File file, long l) throws IOException {
     myOperations.add("SET_LAST_MODIFIED " + unifyPath(file.getPath()));
   }
 
